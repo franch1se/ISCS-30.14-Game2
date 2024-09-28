ea04ca76076f862c7710ddedc37642dae04e0a51
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-09-29 02:29:53 +0800

    more physics

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ const GRAVITY = Vector2(0, 800.0)
 
 var jump_count = 3
 var is_jumping = false
+var is_falling = false
 
 
 func _physics_process(delta: float) -> void:
@@ -16,7 +17,13 @@ func _physics_process(delta: float) -> void:
 	else: 
 		jump_count = 3
 		is_jumping = false
-		
+	
+	# Check if falling (not from jump)
+	if velocity.y > 0 and !is_jumping:
+		is_falling = true
+	else:
+		is_falling = false
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump(JUMP_VELOCITY, 1)
@@ -26,11 +33,11 @@ func _physics_process(delta: float) -> void:
 		jump_count -= 1
 		is_jumping = true
 		
+	# End jump / "smash down"
 	if is_jumping and Input.is_action_just_pressed("down"):
 		velocity.y -= JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Handle input and left/right movement
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		accelerate(direction)
@@ -39,7 +46,7 @@ func _physics_process(delta: float) -> void:
 
 	wall_collision(delta)
 	move_and_slide()
-	
+
 func jump(y_velocity, mult):
 	velocity.y = y_velocity*mult
 
@@ -56,13 +63,12 @@ func decelerate():
 		velocity.x = max(velocity.x - SPEED*0.5, 0)
 	elif velocity.x < 0:
 		velocity.x = min(velocity.x + SPEED*0.5, 0)
-
+		
 func wall_collision(delta) -> void:
 	var collision = move_and_collide(velocity*delta)
-	if collision and !is_jumping:
+	if collision and !is_jumping and !is_falling:
 		if velocity.x > 0:
 			velocity.x = -100
 		else:
 			velocity.x = 100
 		decelerate()
-		
