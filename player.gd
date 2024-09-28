87aa7f451b99ee4456d505c3c894ac65d0db1304
franch1se <ronald.francis.bautista@student.ateneo.edu>
2024-09-28 18:42:08 +0800

    added tilemap w/ collision

--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ const MAX_SPEED = 500
 const GRAVITY = Vector2(0, 800.0)
 
 var jump_count = 3
+var is_jumping = false
 
 
 func _physics_process(delta: float) -> void:
@@ -14,12 +15,19 @@ func _physics_process(delta: float) -> void:
 		velocity += GRAVITY * delta
 	else: 
 		jump_count = 3
+		is_jumping = false
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump(JUMP_VELOCITY, 1)
+		is_jumping = true
 	elif Input.is_action_just_pressed("jump") and !is_on_floor() and jump_count>1:
 		jump(JUMP_VELOCITY, 1)
 		jump_count -= 1
+		is_jumping = true
+		
+	if is_jumping and Input.is_action_just_pressed("down"):
+		velocity.y -= JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
